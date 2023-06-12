29df0620527fa511e67fea117adff140f6a4d9a7
Your Name <you@example.com>
2023-06-13 02:25:15 +0600

    index

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+      return (
+            <div>
+                  <h2>Header Page</h2>
+                
+            </div>
+      );
+};
+
+export default Header;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Home = () => {
+      return (
+            <div className='text-5xl text-red-700  text-center p-5 '>
+                  <h2>Hello Home Page</h2>
+            
+            </div>
+      );
+};
+
+export default Home;
